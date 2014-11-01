bf70a7d6d678569cf5ee8679854cb21b42cca47f
Palmer Dabbelt <palmer.dabbelt@eecs.berkeley.edu>
2014-10-31 19:53:43 -0700

    Don't use valgrind on machines without it
    I don't want to just straight-up fail the tests when there's no
    valgrind...

--- test/hurricane-hexinfo/harness.bash
+++ test/hurricane-hexinfo/harness.bash
@@ -3,12 +3,19 @@ set -ex
 cat input.hex
 cat output.gold
 
-valgrind --quiet --log-file=valgrind.log \
-    $PTEST_BINARY $FORMAT input.hex > output.test
-cat valgrind.log
-if [[ "$(cat valgrind.log | wc -l)" != 0 ]]
+valgrind="$(which valgrind 2> /dev/null || true)"
+if [[ "$valgrind" != "" ]]
 then
-    exit 1
+    valgrind --quiet --log-file=valgrind.log \
+        $PTEST_BINARY $FORMAT input.hex > output.test
+    cat valgrind.log
+    if [[ "$(cat valgrind.log | wc -l)" != 0 ]]
+    then
+        exit 1
+    fi
+
+    diff -u output.gold output.test
 fi
 
+$PTEST_BINARY $FORMAT input.hex > output.test
 diff -u output.gold output.test
